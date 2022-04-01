68286c73f3a71076c30adfd5371eb6d4e248760a
edtimer <26600954+edtimer@users.noreply.github.com>
2022-04-01 12:27:11 +0100

    added register path to index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,24 @@
-console.log("Netzillaa project");
+require("dotenv").config();
+const path = require("path");
+//initalizing express server
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static("../../Frontend/public"));
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+app.get("/register", (req, res) => {
+  var registrationPath = path.join(__dirname + "/../Frontend/public");
+  console.log(path.join(__dirname + "/../Frontend/public/index.html"));
+  res.sendFile(path.join(__dirname + "/../Frontend/public/index.html"));
+});
+
+//setting port
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
