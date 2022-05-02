8583f0db9e00e8fcdabce253d0482e647e186da2
Zeiad-Wael <96629545+Zeiad-Wael@users.noreply.github.com>
2022-05-02 22:18:37 +0800

    fixed dashboard, made the click thing that add it to the cart. can change the products data from GlobalState line 27 from data to DBProducts. have an error in get total

--- Frontend/src/components/pages/dashboard/Cart.js
+++ Frontend/src/components/pages/dashboard/Cart.js
@@ -1,127 +1,76 @@
-import React, { createContext, useReducer, useEffect, useContext, useState } from "react";
+import React, { useRef } from "react";
 import { Scrollbars } from "react-custom-scrollbars-2";
-import "./Cart.css";
-import data from "./data";
-import Items from "./Items";
-import { CartManager } from "./CartManager";
+import '../../../css/Cart.css';
 
-export const CartContext = createContext();
+import useOnClickOutside from "./reducer/useOnClickOutside";
 
+export default function Cart({
+    isToggle,
+    setToggle,
+    carts,
+    removeProductFromCart,
+    deleteProductFromCart,
+    addProductToCart,
+    clearCart,
+}) {
 
+    const boughtProducts = carts.map(({ product, quantity }) => {
+        return (
+            <div>
+                <div
+                    key={product.id}
+                />
+                <div className="items-info">
+                    <div className="title">
+                        <h4>{product.productName}</h4>
+                    </div>
 
-const initialState = {
-    //if item:data it will start with mapping all items
-    item: [],
-    totalAmount: 0,
-    totalItem: 0,
-};
+                    <div className="add-minus-quantity">
+                        <i className="fas fa-minus minus" onClick={() => removeProductFromCart(product.id)}>-</i>
+                        <input type="text" placeholder={quantity} disabled />
+                        <i className="fas fa-plus add" onClick={() => addProductToCart(product)}>+</i>
+                    </div>
 
-export const addToCart = (productID) => {
-    // for (var i = 0; i < data.length; i++) {
-        // if (data[i].id == productID) {
-            console.log("asdasdasda"+productID);
+                    <div className="price">
+                        <h4>RM{product.productPrice * quantity}</h4>
+                    </div>
 
-            // addProductToCart(data[i]);
-        // }
-    // }
-}
-
-const ContextCart = () => {
-    const { clearCart, totalItem, totalAmount, addItem } = useContext(CartContext);
-    const [item, setItems] = useState([]);
-
-    const addProductToCart = (itemInfo) => {
-        for (var i = 0; i < item.length; i++) {
-            if (item[i].id == itemInfo.id) {
-                
-            }
-            setItems([...item, itemInfo])
-        }
-    }
+                    <div className="remove-item">
+                        <i
+                            className="fas fa-trash-alt remove"
+                            onClick={() => deleteProductFromCart(product.id)}> X </i>
+                    </div>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <>
             <section className="main-cart-section">
                 <h6>Shopping Cart</h6>
                 <p className="total-items">
-                    you have <span className="total-items-count">{totalItem} </span> items
+                    you have <span className="total-items-count">{carts.length} </span> items
                     in shopping cart
                 </p>
-
                 <div className="cart-items">
                     <div className="cart-items-container">
                         <Scrollbars>
-                            {item.map((curItem) => {
-                                return <Items key={curItem.id} {...curItem} />;
-                            })}
+                            {boughtProducts}
                         </Scrollbars>
                     </div>
                 </div>
 
                 <div className="card-total">
                     <h3>
-                        Cart Total : <span>RM {totalAmount}</span>
+                        Cart Total : <span>RM {}</span>
                     </h3>
                     <button>checkout</button>
-                    <button className="clear-cart" onClick={clearCart}>
+                    <button className="clear-cart" onClick={() => clearCart()}>
                         Clear Cart
                     </button>
                 </div>
             </section>
         </>
     );
-};
-
-const Cart = () => {
-    // const [item, setItem] = useState(products);
-    const [state, dispatch] = useReducer(CartManager, initialState);
-    const addItem = (id) => {
-        return dispatch({
-            type: "ADD_ITEM",
-            payload: id,
-        });
-    }
-    // to delete the indv. elements from an Item Cart
-    const removeItem = (id) => {
-        return dispatch({
-            type: "REMOVE_ITEM",
-            payload: id,
-        });
-    };
-
-    // clear the cart
-    const clearCart = () => {
-        return dispatch({ type: "CLEAR_CART" });
-    };
-
-    // increment the item
-    const increment = (id) => {
-        return dispatch({
-            type: "INCREMENT",
-            payload: id,
-        });
-    };
-
-    // decrement the item
-    const decrement = (id) => {
-        return dispatch({
-            type: "DECREMENT",
-            payload: id,
-        });
-    };
-
-    // we will use the useEffect to update the data
-    useEffect(() => {
-        dispatch({ type: "GET_TOTAL" });
-        // console.log("Awesome");
-    }, [state.item]);
-
-    return (
-        <CartContext.Provider
-            value={{ ...state, removeItem, clearCart, increment, decrement, addItem }}>
-            <ContextCart />
-        </CartContext.Provider>
-    );
-};
-
-export default Cart;
+}
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../src/css/index.css";
 import App from "./App";
+import GlobalState from "./components/pages/dashboard/reducer/GlobalState";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalState>
+      <App />
+    </GlobalState>
   </React.StrictMode>,
   document.getElementById("root")
 );
